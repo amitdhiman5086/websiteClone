e73b9cfda02e4a7561d5c4ae22eb73b15484d674
Amit <amitdhiman5086@gmail.com>
2024-04-11 14:26:58 +0530

    These About PAge 2

--- index.js
+++ index.js
@@ -1,44 +1,73 @@
-function navAnimation(){
-    var nav = document.querySelector("nav") 
+function navAnimation() {
+    var nav = document.querySelector("nav")
 
-nav.addEventListener("mouseenter" , function(){
+    nav.addEventListener("mouseenter", function () {
 
-    let tl = gsap.timeline()
+        let tl = gsap.timeline()
 
-    tl.to(".nav-bottom" , {
-        height : "21vh"
+        tl.to(".nav-bottom", {
+            height: "21vh"
+        })
+        tl.to(".nav-part2 h5", {
+            display: "block"
+        })
+        tl.to(".nav-part2 h5 span", {
+            y: "0",
+            // duration : 0.3 ,
+            stagger: {
+                amount: 0.6
+            }
+        })
     })
-    tl.to(".nav-part2 h5" , {
-        display : "block"
-    })
-    tl.to(".nav-part2 h5 span" , {
-        y : "0" ,
-        // duration : 0.3 ,
-        stagger : {
-            amount : 0.6
-        }
-    })
-})
-nav.addEventListener("mouseleave" , function(){
+    nav.addEventListener("mouseleave", function () {
 
-    let tl = gsap.timeline()
-    tl.to(".nav-part2 h5 span" , {
-        y : "25" ,
-        // duration : 0.3 ,
-        stagger : {
-            amount : 0.2
-        }
+        let tl = gsap.timeline()
+        tl.to(".nav-part2 h5 span", {
+            y: "25",
+            // duration : 0.3 ,
+            stagger: {
+                amount: 0.2
+            }
+        })
+        tl.to(".nav-part2 h5", {
+            display: "none",
+            duration: 0.1
+        })
+        tl.to(".nav-bottom", {
+            height: "0vh",
+            duration: 0.1
+        })
     })
-    tl.to(".nav-part2 h5" , {
-        display : "none" ,
-        duration : 0.1
+}
+
+
+// navAnimation() ;
+
+function page2Animation(){
+    var relem = document.querySelectorAll(".right-elem");
+
+relem.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+        gsap.to(elem.childNodes[3], {
+            opacity: 1,
+            scale: 1
+        })
     })
-    tl.to(".nav-bottom" , {
-        height : "0vh" ,
-        duration : 0.1
+    elem.addEventListener("mouseleave", function () {
+        gsap.to(elem.childNodes[3], {
+            opacity: 0,
+            scale: 0
+        })
+
+    })
+    elem.addEventListener("mousemove" , function(dets){
+        gsap.to(elem.childNodes[3],{
+            x:dets.x - elem.getBoundingClientRect().x -70,
+            y:dets.y - elem.getBoundingClientRect().y -150, 
+           
+        })
     })
 })
 }
- 
 
-navAnimation()
+// page2Animation() ;
