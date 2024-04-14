7e3c22048751c2253d67ece54b910a048c902f6c
Amit <amitdhiman5086@gmail.com>
2024-04-14 12:27:30 +0530

    Page 3 is Here

--- index.js
+++ index.js
@@ -40,34 +40,59 @@ function navAnimation() {
     })
 }
 
-
-navAnimation() ;
-
 function page2Animation(){
     var relem = document.querySelectorAll(".right-elem");
+    
+    relem.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 1,
+                scale: 1
+            })
+        })
+        elem.addEventListener("mouseleave", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 0,
+                scale: 0
+            })
+            
+        })
+        elem.addEventListener("mousemove" , function(dets){
+            gsap.to(elem.childNodes[3],{
+                x:dets.x - elem.getBoundingClientRect().x -70,
+                y:dets.y - elem.getBoundingClientRect().y -150, 
+                
+            })
+        })
+    })
+}
 
-relem.forEach(function (elem) {
-    elem.addEventListener("mouseenter", function () {
-        gsap.to(elem.childNodes[3], {
-            opacity: 1,
-            scale: 1
-        })
-    })
-    elem.addEventListener("mouseleave", function () {
-        gsap.to(elem.childNodes[3], {
-            opacity: 0,
-            scale: 0
-        })
+function page3Animation(){
+    var page3Centre = document.querySelector("#page3-centre") ;
+var video = document.querySelector("#page-3 video") ;
+console.log(video) ;
 
+page3Centre.addEventListener("click" , function(){
+    video.play() 
+    gsap.to(video,{
+        transform : "scaleX(1) scaleY(1)" ,
+        opacity : 1 ,
+        borderRadius : 0
+        
     })
-    elem.addEventListener("mousemove" , function(dets){
-        gsap.to(elem.childNodes[3],{
-            x:dets.x - elem.getBoundingClientRect().x -70,
-            y:dets.y - elem.getBoundingClientRect().y -150, 
-           
-        })
+})
+video.addEventListener("click" ,function(){
+    video.pause() 
+    gsap.to(video,{
+        transform : "scaleX(0.7) scaleY(0)" ,
+        opacity : 0 ,
+        borderRadius : "30px"
+        
     })
+
 })
 }
 
+navAnimation() ;
 page2Animation() ;
+page3Animation() ;
